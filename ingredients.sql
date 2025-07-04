2e370c5c8fda8dffedc2c698f6fe07bdea209155
SnehaKDuglet <snehaduglet@gmail.com>
2025-07-04 23:59:34 +0530

    Add files via upload
    Welcome to my collection of SQL-based database projects, designed with real-world scenarios in mind. These projects showcase my skills in:
    
    ✅ Database design using normalization and relational modeling
    
    ✅ Writing efficient SQL queries for CRUD operations
    
    ✅ Implementing joins, subqueries, grouping, and constraints
    
    ✅ Handling user interaction, feedback systems, reservations, and more
    
    ✅ Building data-driven backend logic ready for integration with applications

---
+++ ingredients.sql
@@ -0,0 +1,35 @@
+CREATE TABLE ingredients (
+    ingredient_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+select * from ingredients
+
+INSERT INTO ingredients
+VALUES
+(1, 'Beans'),
+(2, 'Brown Rice'),
+(3, 'Butter'),
+(4, 'Cheese'),
+(5, 'Chickpeas'),
+(6, 'Cucumber'),
+(7, 'Eggs'),
+(8, 'Feta Cheese'),
+(9, 'Flattened Rice'),
+(10, 'Flour'),
+(11, 'Kidney Beans'),
+(12, 'Milk'),
+(13, 'Mixed Vegetables'),
+(14, 'Oats'),
+(15, 'Onions'),
+(16, 'Paneer'),
+(17, 'Pecans'),
+(18, 'Potatoes'),
+(19, 'Rice'),
+(20, 'Soy Sauce'),
+(21, 'Spices'),
+(22, 'Sugar'),
+(23, 'Tomatoes'),
+(24, 'Tortillas'),
+(25, 'Wheat Flour');
+
